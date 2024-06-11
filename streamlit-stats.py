087764be9e545a79b1eb4e3d5c8ff599be65d571
Brent Bundang <brent.bundang@gmail.com>
2024-06-10 21:58:42 -0400

    initial commit

---
+++ streamlit-stats.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd 
+
+season_by_season_stats = pd.read_csv("player-totals.csv")
+
+start_year = st.slider("Select the start year", 1974, 2023, 1974)
+end_year = st.slider("Select the end year", 1974, 2024, 2024)
+
+last_ten_years = season_by_season_stats[season_by_season_stats['season'].between(start_year,end_year)]
+
+st.write(f'Compare season stats from {start_year}-{end_year} season')
+options = st.multiselect(
+     "Select three players to compare", last_ten_years['player'].drop_duplicates().tolist(), max_selections=3)
+
+point_totals = last_ten_years[['season', 'player', 'pts']]
+
+selected_players = point_totals[point_totals['player'].isin(options)]
+selected_players['season'] = selected_players['season'].astype(str)
+
+st.title(f'Points from {start_year}-{end_year} season')
+st.line_chart(selected_players, x='season', y='pts', color='player')
