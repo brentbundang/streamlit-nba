b8a9c0eadee7463639a259a4e1c14cb3380cba80
Brent Bundang <brent.bundang@gmail.com>
2024-06-10 22:09:31 -0400

    Rename variable from last ten to generic

--- streamlit-stats.py
+++ streamlit-stats.py
@@ -6,13 +6,13 @@ season_by_season_stats = pd.read_csv("player-totals.csv")
 start_year = st.slider("Select the start year", 1974, 2023, 1974)
 end_year = st.slider("Select the end year", 1974, 2024, 2024)
 
-last_ten_years = season_by_season_stats[season_by_season_stats['season'].between(start_year,end_year)]
+selected_years = season_by_season_stats[season_by_season_stats['season'].between(start_year,end_year)]
 
 st.write(f'Compare season stats from {start_year}-{end_year} season')
 options = st.multiselect(
-     "Select three players to compare", last_ten_years['player'].drop_duplicates().tolist(), max_selections=3)
+     "Select three players to compare", selected_years['player'].drop_duplicates().tolist(), max_selections=3)
 
-point_totals = last_ten_years[['season', 'player', 'pts']]
+point_totals = selected_years[['season', 'player', 'pts']]
 
 selected_players = point_totals[point_totals['player'].isin(options)]
 selected_players['season'] = selected_players['season'].astype(str)
